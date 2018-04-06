a54e9a16d6e0791d5c3d748d2b244a1f85959871
Eldos <eldosnarbay@gmail.com>
2018-04-06 16:56:37 +0600

    Java based configuration

---
+++ src/main/java/kz/eldos/testapp/Config.java
@@ -0,0 +1,42 @@
+package kz.eldos.testapp;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackageClasses = TestappApplication.class)
+public class Config {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/user_db?useSSL=false");
+        dataSource.setUsername("root");
+        dataSource.setPassword("123qwe123");
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean configureEntityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(dataSource());
+        entityManagerFactoryBean.setPackagesToScan("kz.eldos");
+        entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+
+        Properties jpaProperties = new Properties();
+        jpaProperties.put(org.hibernate.cfg.Environment.DIALECT, "org.hibernate.dialect.MySQL5InnoDBDialect");
+        entityManagerFactoryBean.setJpaProperties(jpaProperties);
+
+        return entityManagerFactoryBean;
+    }
+}
--- src/main/java/kz/eldos/testapp/TestappApplication.java
+++ src/main/java/kz/eldos/testapp/TestappApplication.java
@@ -3,10 +3,17 @@ package kz.eldos.testapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication
 public class TestappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestappApplication.class, args);
+        SpringApplication application = new SpringApplication(TestappApplication.class);
+        Map<String, Object> props = new HashMap<>();
+        props.put("server.port", 8085);
+        application.setDefaultProperties(props);
+		application.run(args);
 	}
 }
